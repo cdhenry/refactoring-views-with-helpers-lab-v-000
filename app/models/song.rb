dbd6abb8a2aea5f688f3d3dd606193b7be5f8fcf
Chris Henry <cdhenry@gmail.com>
2018-04-04 13:43:47 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,6 +10,6 @@ class Song < ActiveRecord::Base
   def artist_name=(name)
     artist = Artist.find_or_create_by(name: name)
     self.artist = artist
-    self.artist.name = name
+    self.artist.name
   end
 end
