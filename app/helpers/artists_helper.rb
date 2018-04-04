146585dfee2ae15167013e62d94f28953e8be06a
Chris Henry <cdhenry@gmail.com>
2018-04-04 13:21:30 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,4 +1,8 @@
 module ArtistsHelper
-  def display_artist
+  def display_artist(artist)
+    if artist.song != nil
+      link_to "#{artist.name}", artist_path(artist) 
+    else
+      link_to "Add Artist", 'song/edit'
   end
 end
