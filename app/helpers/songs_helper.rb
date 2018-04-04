c834a0698680f60808c0dd6327239d9d9959e7e2
Chris Henry <cdhenry@gmail.com>
2018-04-04 13:32:02 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,7 +1,7 @@
 module SongsHelper
   def display_artist(song)
     if song.artist_name
-      link_to "#{song.artist.name}", song.artist
+      link_to "#{song.artist_name}", song.artist
     else
       link_to "Add Artist", edi_song_path(song)
     end
